58116f43813638be39672cc6dc2ef121c5e54a2d
petrivmykola3@gmil.com <koljapetriw05031992>
2018-03-10 13:11:56 +0200

    final version of game

--- src/main/java/net/petriv/play/Solution.java
+++ src/main/java/net/petriv/play/Solution.java
@@ -5,25 +5,28 @@ import java.util.*;
 public class Solution {
 
     static int jumpingOnClouds(int[] c) {
-        int lenght = c.length - 1;
+
+        int amountOfClouds = c.length - 1;
         int jump = 0;
 
-        for (int i = 0; i < lenght & (i + 2) <= lenght; ) {
+        if (amountOfClouds == 1) return 1; // if only 2 clouds return 1 jump
 
-            if (c[i + 2] == 0) {
-                jump++;
-                i+=2;
+        for (int i = 0; (i + 2) <= amountOfClouds; ) {
+
+            if (c[i + 2] == 0) { // first try to jump Cn+2
+                jump++;          //increse amount jumping
+                i += 2;           // jump on cloud
+
+                if ((amountOfClouds - 1) != i) { // if we are on last but one cloud just one jump
+                    continue;
+                } else jump++;
+
+            } else if (c[i + 1] == 0) {  // jump to Cn+1
+                jump++;                  //increse amount jumping
+                i++;                     //jump on cloud
                 continue;
-
-            } else if (c[i + 1] == 0) {
-                jump++;
-                i++;
-                continue;
-
-            } else return -1;
-
+            } else return -1; // -1 means lose the game
         }
-
         return jump;
     }
 
@@ -34,8 +37,8 @@ public class Solution {
         int[] c = new int[n];
 
         //Cloud C0 and Cn-1 by default 0
-        System.out.println("Please enter " + n + " describing clouds '0 ordinary' '1 thundercloud' ");
-        for (int c_i = 0; c_i < n; c_i++) {
+        System.out.println("Please enter " + (n - 2) + " describing clouds '0 ordinary' '1 thundercloud'  ");
+        for (int c_i = 0; c_i < n - 2; c_i++) {
             c[c_i] = in.nextInt();
         }
         int result = jumpingOnClouds(c);
--- src/test/java/net/petriv/play/SolutionTest.java
+++ src/test/java/net/petriv/play/SolutionTest.java
@@ -4,19 +4,29 @@ import static org.junit.Assert.*;
 
 public class SolutionTest {
 
-    int[] clouds = new int[7];
+    int[] tenClouds = new int[10];
+    int[] sevenClouds = new int[7];
+    int[] twoClouds = new int[2];
 
 
     @org.junit.Before
     public void setUp() throws Exception {
-        clouds = new int[]{0, 0, 1, 0, 0, 1, 0};
+        tenClouds = new int[]{0, 0, 1, 0, 0, 1, 0, 1, 0, 0};
+        sevenClouds = new int[]{0, 0, 1, 0, 1, 0, 0};
+        twoClouds = new int[] {0, 0};
     }
 
     @org.junit.Test
-    public void jumpingOnClouds() {
-        assertEquals(4, Solution.jumpingOnClouds(clouds));
+    public void jumpingOnTenClouds() {
+        assertEquals(6, Solution.jumpingOnClouds(tenClouds));
+    }
 
-
-
+    @org.junit.Test
+    public void jumpingOnSevenClouds() {
+        assertEquals(4, Solution.jumpingOnClouds(sevenClouds));
+    }
+    @org.junit.Test
+    public void jumpingOnTwoClouds() {
+        assertEquals(1, Solution.jumpingOnClouds(twoClouds));
     }
 }
